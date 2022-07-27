95da5fe154a84f875bd52eb0858eb78cfa7ba767
genas86 <sgm86@bk.ru>
2022-07-27 16:34:34 +0300

    Initial Commit

---
+++ DZ2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ2.01
+//
+//  Created by Genas1986 on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
